e9807d4061b1935ba0299ca011be2abfddf3f9ca
a616707902 <616707902@qq.com>
2018-08-22 17:43:31 +0800

    角色管理

--- pages/permissions/roleManage/RoleAdd.js
+++ pages/permissions/roleManage/RoleAdd.js
@@ -1,6 +1,6 @@
 layui.extend({
     request: '{/}../../../static/js/network/request',
-    authtree: '{/}../../../lib/layui/lay/anthtree'
+    authtree: '{/}../../../lib/layui/lay/authtree'
 });
 layui.use(['layer', 'request', 'jquery', 'form', 'authtree'], function () {
     var usertable = layui.data('user');
@@ -8,14 +8,69 @@ layui.use(['layer', 'request', 'jquery', 'form', 'authtree'], function () {
     var layer = layui.layer;
     var $ = layui.jquery;
     var form = layui.form;
+    var authtree=layui.authtree;
     var request = layui.request;
     form.render();
     $(function () {
 
         request.doGet("MenuOperation/getOperationMenuList", {username: user.username}, function (data) {
 
+            var nodes = new Array();
+
+            for (var i = 0; i < data.length; i++) {
+//&& data[i].levelnum == 1
+                if (data[i].pareMenuId == 0 ) {
+                    var list2 = new Array();
+                    var menuOne = new Object();
+                    menuOne.value = data[i].menuId;
+                    menuOne.name = data[i].menuName;
+//&& data[j].levelnum == 2
+                    for (var j = 0; j < data.length; j++) {
+                        if (data[j].pareMenuId == data[i].menuId ) {
+                            var list3 = new Array();
+                            var menuTwo = new Object();
+                            menuTwo.value = data[j].menuId;
+                            menuTwo.name = data[j].menuName;
+                            for (var k = 0; k < data.length; k++) {
+                                //&& data[k].levelnum == 3
+                                if (data[k].pareMenuId == data[j].menuId ) {
+                                    var menuThree = new Object();
+                                    menuThree.value = data[k].menuId;
+                                    menuThree.name = data[k].menuName;
+                                    list3.push(menuThree);
+                                }
+                            }
+                            menuTwo.list = list3;
+                            list2.push(menuTwo);
+                        }
+
+                    }
+                    menuOne.list = list2;
+                    nodes.push(menuOne);
+                }
+
+            }
+            authtree.render('#LAY-auth-tree-index', nodes, {inputname: 'authids[]', layfilter: 'lay-check-auth', openall: false});
+
         });
 
     });
-
+    // 异步提交表单信息
+    form.on('submit()', function(obj){
+        // 注意如果直接通过layui的obj.field可能会出现问题，layuiAdmin请使用admin.req();
+        var authids = authtree.getAll('#LAY-auth-tree-index');
+        console.log('Choosed authids is', authids);
+        request.doPost("addRole",{
+            rolename:obj.field.rolename,
+            describe:obj.field.describe,
+            state:obj.field.state,
+            rolemenus:authids
+        },function (data) {
+            parent.reflush();
+            // 获得frame索引
+            var index = parent.layer.getFrameIndex(window.name);
+            //关闭当前frame
+            parent.layer.close(index);
+        });
+    });
 });
--- pages/permissions/roleManage/role_manage.js
+++ pages/permissions/roleManage/role_manage.js
@@ -1,3 +1,44 @@
 layui.extend({
     request: '{/}../../../static/js/network/request',
 });
+
+
+layui.use(['request','jquery'],function () {
+    window.reflush = function () {
+        window.parent.location.reload(); //刷新父页面
+        location.replace(location.href);
+    }
+    window.AddRoleShow=function (title, url,  w, h) {
+
+        if (title == null || title == '') {
+            title = false;
+        }
+        ;
+        if (url == null || url == '') {
+            url = "/HotelAdmin/pages/404.html";
+        }
+        ;
+        if (w == null || w == '') {
+            w = ($(window).width() * 0.9);
+        }
+        ;
+        if (h == null || h == '') {
+            h = ($(window).height() - 50);
+        }
+        ;
+        layer.open({
+            type: 2,
+            area: [w + 'px', h + 'px'],
+            fix: false, //不固定
+            maxmin: true,
+            shadeClose: true,
+            shade: 0.4,
+            title: title,
+            content: url,
+            success: function (layero, index) {
+                var body = layui.layer.getChildFrame('body', index);
+            }
+        });
+    }
+
+});
